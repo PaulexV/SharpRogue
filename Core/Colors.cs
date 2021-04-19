6511d56140d868a69bbfaf9a2c55fcb0b8eefdc4
PaulexV <paulalexis.verrier@ynov.com>
2021-04-19 15:50:53 +0200

    initial commit

---
+++ Core/Colors.cs
@@ -0,0 +1,20 @@
+using RLNET;
+
+namespace sharpRogue.Core
+{
+    public class Colors
+    {
+        public static RLColor FloorBackground = RLColor.Black;
+        public static RLColor Floor = Palette.AlternateDarkest;
+        public static RLColor FloorBackgroundFov = Palette.DbDark;
+        public static RLColor FloorFov = Palette.Alternate;
+
+        public static RLColor WallBackground = Palette.SecondaryDarkest;
+        public static RLColor Wall = Palette.Secondary;
+        public static RLColor WallBackgroundFov = Palette.SecondaryDarker;
+        public static RLColor WallFov = Palette.SecondaryLighter;
+
+        public static RLColor TextHeading = Palette.DbLight;
+
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,87 @@
+﻿using RLNET;
+using sharpRogue.Core;
+
+namespace sharpRogue
+{
+    public class Game
+    {
+        // The screen height and width are in number of tiles
+        private static readonly int _screenWidth = 200;
+        private static readonly int _screenHeight = 110;
+
+        private static RLRootConsole _rootConsole;
+
+        // The map console takes up most of the screen and is where the map will be drawn
+        private static readonly int _mapWidth = 160;
+        private static readonly int _mapHeight = 96;
+        private static RLConsole _mapConsole;
+
+        // Below the map console is the message console which displays attack rolls and other information
+        private static readonly int _messageWidth = 160;
+        private static readonly int _messageHeight = 22;
+        private static RLConsole _messageConsole;
+
+        // The stat console is to the right of the map and display player and monster stats
+        private static readonly int _statWidth = 40;
+        private static readonly int _statHeight = 140;
+        private static RLConsole _statConsole;
+
+        // Above the map is the inventory console which shows the players equipment, abilities, and items
+        private static readonly int _inventoryWidth = 160;
+        private static readonly int _inventoryHeight = 22;
+        private static RLConsole _inventoryConsole;
+
+        public static void Main()
+        {
+            // This must be the exact name of the bitmap font file we are using or it will error.
+            string fontFileName = "terminal8x8.png";
+            // The title will appear at the top of the console window
+            string consoleTitle = "RougeSharp V3 Tutorial - Level 1";
+            // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight,
+              8, 8, 1f, consoleTitle);
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+            // Set up a handler for RLNET's Update event
+            _rootConsole.Update += OnRootConsoleUpdate;
+            // Set up a handler for RLNET's Render event
+            _rootConsole.Render += OnRootConsoleRender;
+            // Begin RLNET's game loop
+            _rootConsole.Run();
+        }
+
+        // Event handler for RLNET's Update event
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorBackground);
+            _mapConsole.Print(1, 1, "Map", Colors.TextHeading);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Palette.DbDeepWater);
+            _messageConsole.Print(1, 1, "Messages", Colors.TextHeading);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Palette.DbOldStone);
+            _statConsole.Print(1, 1, "Stats", Colors.TextHeading);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Palette.DbWood);
+            _inventoryConsole.Print(1, 1, "Inventory", Colors.TextHeading);
+        }
+
+        // Event handler for RLNET's Render event
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            // Blit the sub consoles to the root console in the correct locations
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
+              _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight,
+              _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
+              _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight,
+              _rootConsole, 0, 0);
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
+        }
+    }
+}
